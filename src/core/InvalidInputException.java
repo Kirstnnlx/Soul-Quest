81f249908f9116390ad71112a15073d76421daf9
Kirsten Roan 𐙚 <Prettykirstenroan@gmail.com>
2025-11-30 15:24:39 +0800

    Create InvalidInputException class
    Add custom exception for invalid input with error code.

--- src/core/InvalidInputException.java
+++ src/core/InvalidInputException.java
@@ -1,1 +1,26 @@
+package core;
 
+public class InvalidInputException extends RuntimeException {
+    private String inputValue;
+    private int errorCode;
+
+    public InvalidInputException(String message) {
+        super(message);
+        this.inputValue = "";
+        this.errorCode = 400;
+    }
+
+    public InvalidInputException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return "✖ " + super.getMessage();
+    }
+
+    public String getInputValue() { 
+        return inputValue; 
+    }
+}
